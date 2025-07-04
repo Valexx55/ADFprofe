ac479ee1cfa4ac069e335217cd78b025aa0de8ed
valexx55 <valexx55@gmail.com>
2025-07-04 13:15:43 +0200

    activity IMC casi terminada

--- app/src/main/java/edu/adf/profe/ImcActivity.kt
+++ app/src/main/java/edu/adf/profe/ImcActivity.kt
@@ -3,6 +3,7 @@ package edu.adf.profe
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +11,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ImcActivity : AppCompatActivity() {
+
+    var numeroVecesBoton: Int = 0 //PARA LLEVAR LA CUENTA de veces que el usuario toca el botón
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,19 +26,84 @@ class ImcActivity : AppCompatActivity() {
     }
 
     fun calcularImc(view: View) {
-        //LISTENER / CALLBACK
-        //TODO hacer el cálculo del IMC
+
         Log.d("MIAPP", "El usuario ha tocado el botón de calcular IMC")
-        //creamos la notificación/mensaje
-        val toast:Toast = Toast.makeText(this, "El usuario ha tocado el botón de calcular IMC", Toast.LENGTH_LONG)
-        //mostrar la notificación
-        toast.show()
-        //cerrar la activity (si hubiera una ventana/activity apilada -detrás-, se mostraría. Si no, te sales de la app)
-        finish()
-        //salir de la app / cerrar todas las ventanas
-        finishAffinity()
+        informarNumVecesBoton()
+        //IMC = peso / (altura al cuadrado)  DIVISIÓN --> / multiplicación *
+        //1 obtener el peso introducido
+        val peso:Float = obtenerPeso()
+        Log.d("MIAPP", "PESO introducido $peso")
+        //2 obtener la altura introducida
+        val altura = obtenerAltura()
+        Log.d("MIAPP", "ALTURA introducido $altura")
+        //3 hacer la fórmula del imc - PARÁMETROS ACTUALES
+        val resultadoImc : Float = calculoImcNumerico(peso, altura) //LLAMADA/INVOKE - debe haber una coincidencia entre el tipo, número y el orden de los parámetros/variables
+        Log.d("MIAPP", "ALTURA introducido $resultadoImc")
+        //4 mostrar el resultado
+        mostrarResultado(resultadoImc)
+
 
     }
+
+    fun mostrarResultado(resultadoImc: Float) //: Unit - void no devuelvo nada - no tengo return
+    {
+        val mensaje: Toast = Toast.makeText(this, "SU IMC ES $resultadoImc", Toast.LENGTH_LONG)
+        mensaje.show()
+    }
+
+    //PARÁMETROS FORMALES
+    fun calculoImcNumerico(peso:Float, altura:Float): Float //CABECERA
+    {
+        //1 declaro una variable del tipo devuelto
+        var imc:Float = 0f
+
+            imc = peso / (altura*altura)
+
+        return imc
+    }
+
+    fun obtenerPeso (): Float
+    {
+        //uso var cuando la variable decalarada puede cambiar su valor después (mutable)
+        var peso:Float = 0f //inicializo la variable del peso
+
+            //tengo que coger el valor de la caja
+            //obtener la caja
+            //cuando tenga una variable/dato que no cambia su valor, uso val
+            val editTextPeso: EditText = findViewById<EditText>(R.id.cajaPeso)
+            //"33" text --> 33.0 -- float
+            peso = editTextPeso.text.toString().toFloat() //accedo al contenido y lo traduzco a número
+
+
+        return peso
+    }
+
+
+    fun obtenerAltura (): Float
+    {
+        //uso var cuando la variable decalarada puede cambiar su valor después (mutable)
+        var altura:Float = 0f //inicializo la variable del peso
+
+        //tengo que coger el valor de la caja
+        //obtener la caja
+        //cuando tenga una variable/dato que no cambia su valor, uso val
+        val editTextAltura: EditText = findViewById<EditText>(R.id.cajaAltura)
+        //"33" text --> 33.0 -- float
+        altura = editTextAltura.text.toString().toFloat() //accedo al contenido y lo traduzco a número
+
+
+        return altura
+    }
+
+    fun informarNumVecesBoton ()
+    {
+        numeroVecesBoton = numeroVecesBoton+1; // + string concatenar --> unir palabras // + números sumar
+        val toast:Toast = Toast.makeText(this, "El usuario ha tocado el botón de calcular IMC $numeroVecesBoton veces ", Toast.LENGTH_LONG)
+        //mostrar la notificación
+        toast.show()
+    }
+
+
 
     /**
      *
