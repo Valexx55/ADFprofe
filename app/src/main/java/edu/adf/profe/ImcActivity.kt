b181e694d5429d5b4d880986b75994c72c92406d
valexx55 <valexx55@gmail.com>
2025-07-04 10:18:08 +0200

    finish y Toast

--- app/src/main/java/edu/adf/profe/ImcActivity.kt
+++ app/src/main/java/edu/adf/profe/ImcActivity.kt
@@ -3,6 +3,7 @@ package edu.adf.profe
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,8 +22,26 @@ class ImcActivity : AppCompatActivity() {
     }
 
     fun calcularImc(view: View) {
+        //LISTENER / CALLBACK
         //TODO hacer el cálculo del IMC
         Log.d("MIAPP", "El usuario ha tocado el botón de calcular IMC")
+        //creamos la notificación/mensaje
+        val toast:Toast = Toast.makeText(this, "El usuario ha tocado el botón de calcular IMC", Toast.LENGTH_LONG)
+        //mostrar la notificación
+        toast.show()
+        //cerrar la activity (si hubiera una ventana/activity apilada -detrás-, se mostraría. Si no, te sales de la app)
+        finish()
+        //salir de la app / cerrar todas las ventanas
+        finishAffinity()
 
     }
+
+    /**
+     *
+     * public static Toast makeText(
+     *     android.content.Context context,
+     *     CharSequence text,
+     *     int duration
+     * )
+     */
 }
