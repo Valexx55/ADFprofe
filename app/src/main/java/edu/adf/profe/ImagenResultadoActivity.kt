deb98779d00581795c3d59d4f38e85ce061451dd
valexx55 <valexx55@gmail.com>
2025-07-09 10:36:33 +0200

    RESULTADO IMAGEN COMPLETADO

--- app/src/main/java/edu/adf/profe/ImagenResultadoActivity.kt
+++ app/src/main/java/edu/adf/profe/ImagenResultadoActivity.kt
@@ -3,6 +3,7 @@ package edu.adf.profe
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,10 +19,21 @@ class ImagenResultadoActivity : AppCompatActivity() {
         cambiarTextoEImagen(resultado!!)
     }
 
-    fun cambiarTextoEImagen (resultado:String)
+    fun cambiarTextoEImagen(resultado:String):Unit
     {
-        //TODO completar esta función para que muestre una imagen y un texto adecuados al string resultado recibido
         val imageView:ImageView = findViewById<ImageView>(R.id.fotoResultado)
-        imageView.setImageResource(R.drawable.imc_obeso)
+
+        var imagenImc:Int = when (resultado) {
+            "DESNUTRIDO" -> R.drawable.imc_desnutrido
+            "DELGADO" -> R.drawable.imc_delgado
+            "IDEAL" -> R.drawable.imc_ideal
+            "SOBREPESO" -> R.drawable.imc_sobrepeso
+            "OBESO" ->  R.drawable.imc_obeso
+            else -> R.drawable.imc_desnutrido
+        }
+        imageView.setImageResource(imagenImc)
+
+        val tvLeyenda: TextView = findViewById<TextView>(R.id.leyenda)
+        tvLeyenda.text = resultado
     }
 }
