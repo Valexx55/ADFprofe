c9cbf57c6eb3ae5577d843a2428f6056fa823be5
valexx55 <valexx55@gmail.com>
2025-07-11 10:13:40 +0200

    activity adivina 3/4

--- app/src/main/java/edu/adf/profe/AdivinaNumeroActivity.kt
+++ app/src/main/java/edu/adf/profe/AdivinaNumeroActivity.kt
@@ -4,6 +4,10 @@ package edu.adf.profe
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -29,17 +33,68 @@ class AdivinaNumeroActivity : AppCompatActivity() {
 
     var numeroSecreto:Int = 0
     var numeroVidas: Int = 5
+    var haGanado: Boolean = false //variables miembro/"globales"
+    lateinit var cajaNumeroUsuario: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_adivina_numero)
+        setContentView(R.layout.activity_adivina_numero) // hasta que no se ejecuta esta instrucción, no puedo referirme a ninguna
+        this.cajaNumeroUsuario = findViewById<EditText>(R.id.numeroUsuario)
         numeroSecreto = generarNumeroSecreto()
 
+    }
+
+    fun informarMenor ()
+    {
+        Toast.makeText(this, "El número buscado es menor", Toast.LENGTH_LONG).show()
+    }
+
+    fun informarMayor ()
+    {
+        Toast.makeText(this, "El número buscado es mayor", Toast.LENGTH_LONG).show()
     }
 
     fun intentoAdivina(view: View) {
         Log.d ("MIAPP", "El usuario ha dado a probar")
         //TODO continuar con la APP
+        //val cajaNumUsuario = findViewById<EditText>(R.id.numeroUsuario)
+        val numeroUsuario = this.cajaNumeroUsuario.text.toString().toInt()
+        when {
+            numeroUsuario > numeroSecreto -> informarMenor()
+            numeroUsuario < numeroSecreto -> informarMayor ()
+            else -> {
+                ganador()
+                haGanado = true
+            }
+        }
+        if (!haGanado) //haGando == false
+        {
+            Log.d ("MIAPP", "El usuario no ha acertado")
+            this.numeroVidas = this.numeroVidas-1
+            findViewById<TextView>(R.id.numVidas).text = "$numeroVidas VIDAS"
+            if (this.numeroVidas==0)
+            {
+                informarGameOver()
+            }
+        }
+
+    }
+
+    fun informarGameOver()
+    {
+        val cajaMensajeFinal =  findViewById<TextView>(R.id.textoFinal)
+        cajaMensajeFinal.text = "HAS PERDIDO, EL NÚMERO BUSCADO ERA $numeroSecreto"
+        cajaMensajeFinal.visibility = View.VISIBLE
+        findViewById<Button>(R.id.botonJugar).isEnabled = false
+
+    }
+
+    fun ganador ()
+    {
+        val cajaMensajeFinal =  findViewById<TextView>(R.id.textoFinal)
+        cajaMensajeFinal.text = "HAS ACERTADO, ENHORABUENA"
+        cajaMensajeFinal.visibility = View.VISIBLE
+        findViewById<Button>(R.id.botonJugar).isEnabled = false
     }
 
     fun generarNumeroSecreto(): Int
