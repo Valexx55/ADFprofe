83c909870ed1b424c25c0d7f30ff76d660f48770
Val <valexx55@gmail.com>
2025-09-01 13:34:20 +0200

    ejercicio versiones

--- app/src/main/java/edu/adf/profe/VersionActivity.kt
+++ app/src/main/java/edu/adf/profe/VersionActivity.kt
@@ -1,7 +1,10 @@
 package edu.adf.profe
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,6 +24,11 @@ class VersionActivity : AppCompatActivity() // esto es un comentario
 
     //ATRIBUTOS / campos
     //MÉTODOS / FUNCIONES
+    //Build.VERSION.RELEASE - NÚMERO DE VERSION DE ANDROID (PARA USUARIOS)
+    //Build.VERSION.SDK_INT - VERSIÓN DEL API (MÁS TÉCNICO)
+    //Build.VERSION.CODENAME - NOMBRE CLAVE/ "DE GUERRA"
+
+    val infoVersion =  " (${Build.VERSION.RELEASE})\nAPI ${Build.VERSION.SDK_INT} "
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(Constantes.ETIQUETA_LOG, "Entrando en oncreate ()")
@@ -35,4 +43,66 @@ class VersionActivity : AppCompatActivity() // esto es un comentario
 
         Log.d(Constantes.ETIQUETA_LOG, "Saliendo de oncreate ()")
     }
+
+    fun saludoTocado(view: View) {
+        findViewById<TextView>(R.id.mensajeSalida).text = "HOLA MUNDO"
+    }
+    fun informarVersion(view: View) {
+
+        findViewById<TextView>(R.id.mensajeSalida).text = getAndroidCodename(Build.VERSION.SDK_INT) + infoVersion
+
+        /**
+         * if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+         *     // Código solo para Android 13 o superior
+         * } else {
+         *     // Código para versiones anteriores
+         * }
+         *
+         */
+
+    }
+
+
+    fun getAndroidCodename(sdkInt: Int): String {
+        return when (sdkInt) {
+            36 -> "Baklava"            // Android 16
+            35 -> "VanillaIceCream"    // Android 15
+            34 -> "Upside Down Cake"   // Android 14
+            33 -> "Tiramisu"           // Android 13
+            32 -> "Android 12L"        // Android 12L
+            31 -> "Snow Cone"          // Android 12
+            30 -> "Red Velvet Cake"    // Android 11
+            29 -> "Quince Tart"        // Android 10
+            28 -> "Pie"                // Android 9
+            27 -> "Oreo"               // Android 8.1
+            26 -> "Oreo"               // Android 8.0
+            25 -> "Nougat"             // Android 7.1
+            24 -> "Nougat"             // Android 7.0
+            23 -> "Marshmallow"
+            22 -> "Lollipop"
+            21 -> "Lollipop"
+            20 -> "KitKat Watch"
+            19 -> "KitKat"
+            18 -> "Jelly Bean"
+            17 -> "Jelly Bean"
+            16 -> "Jelly Bean"
+            15 -> "Ice Cream Sandwich"
+            14 -> "Ice Cream Sandwich"
+            13 -> "Honeycomb"
+            12 -> "Honeycomb"
+            11 -> "Honeycomb"
+            10 -> "Gingerbread"
+            9 -> "Gingerbread"
+            8 -> "FroYo"
+            7 -> "Eclair"
+            6 -> "Eclair"
+            5 -> "Eclair"
+            4 -> "Donut"
+            3 -> "Cupcake"
+            2 -> "Banana Bread"
+            1 -> "Apple Pie"
+            else -> "Unknown"
+        }
+    }
+
 }
