cf5640bbef6d7cdcd47898d7b56f3163d998a706
Val <valexx55@gmail.com>
2025-09-05 10:16:12 +0200

    Ordenación múltiple y medición tiempos

--- app/src/main/java/edu/adf/profe/lista/ListaUsuariosActivity.kt
+++ app/src/main/java/edu/adf/profe/lista/ListaUsuariosActivity.kt
@@ -1,6 +1,7 @@
 package edu.adf.profe.lista
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,9 +10,12 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import edu.adf.profe.Constantes
 import edu.adf.profe.R
 import edu.adf.profe.Usuario
 import edu.adf.profe.databinding.ActivityListaUsuariosBinding
+import kotlin.system.measureNanoTime
+import kotlin.time.measureTime
 
 class ListaUsuariosActivity : AppCompatActivity() {
 
@@ -19,7 +23,11 @@ class ListaUsuariosActivity : AppCompatActivity() {
         Usuario("JuanMa", 45, 'M', true, R.color.miazul),
         Usuario("Cristina", 56, 'F', true, R.color.micolorsecundario),
         Usuario("Patri", 46, 'F', true, R.color.minaranja),
-        Usuario("JoseAntonio", 53, 'M', true, R.color.mirojo))
+        Usuario("JoseAntonio", 53, 'M', true, R.color.mirojo),
+        Usuario("Marcos", 31, 'M', true, R.color.gris),
+        Usuario("Jorge", 18, 'M', true, R.color.azulclaro),
+        Usuario("Jorge", 33, 'M', true, R.color.white)
+    )
 
     lateinit var binding: ActivityListaUsuariosBinding
 
@@ -39,15 +47,35 @@ class ListaUsuariosActivity : AppCompatActivity() {
 
     fun ordenarPorNombre(view: View) {
         //this.listaUsuarios.sortedBy { usuario -> usuario.nombre }
-        this.listaUsuarios = this.listaUsuarios.sortedBy { it.nombre }
+
+        var ns = measureNanoTime {  this.listaUsuarios = this.listaUsuarios.sortedBy { it.nombre }}
+        Log.d(Constantes.ETIQUETA_LOG, "EN ORDENAR POR NOMBRE TARDA ${ns} nanosegundos")
+
         //this.listaUsuarios = this.listaUsuarios.sortedByDescending { it.nombre }//de mayor a menor
         this.binding.recViewUsuarios.adapter = UsuariosAdapter(this.listaUsuarios)
     }
     fun ordenarPorEdad(view: View) {
-        this.listaUsuarios = this.listaUsuarios.sortedWith { usuario0, usuario1 -> usuario0.edad - usuario1.edad }
+        var ns1 = measureNanoTime { this.listaUsuarios = this.listaUsuarios.sortedWith { usuario0, usuario1 -> usuario0.edad - usuario1.edad } }
+        var ns2 = measureNanoTime {  this.listaUsuarios = this.listaUsuarios.sortedByDescending { it.edad } }
+
+        Log.d(Constantes.ETIQUETA_LOG, "EN ORDENAR CON WITH TARDA ${ns1} nanosegundos y con BY ${ns2}")
+
         this.binding.recViewUsuarios.adapter = UsuariosAdapter(this.listaUsuarios)
 
     }
 
-    //TODO ordenar por nombre y edad
+    fun ordenarPorNombreYEdad(view: View) {
+
+        var ns = measureNanoTime {
+        this.listaUsuarios = this.listaUsuarios.sortedWith(
+            compareBy<Usuario> { it.nombre }.
+            thenBy { it.edad }
+
+                //.thenByDescending { it.edad }
+        )}
+        Log.d(Constantes.ETIQUETA_LOG, "EN ORDENAR POR NOMBRE y edad TARDA ${ns} nanosegundos")
+        this.binding.recViewUsuarios.adapter = UsuariosAdapter(this.listaUsuarios)
+    }
+
+
 }
