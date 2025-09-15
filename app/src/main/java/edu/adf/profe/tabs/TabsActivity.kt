4ed563bedd700e5c83b1106f9a2f2d84cbbae2a8
valexx55 <valexx55@gmail.com>
2025-09-15 13:46:45 +0200

    OnBackPressed Botón Hacia atrás y ViewPager2 Desliz Vertical

--- app/src/main/java/edu/adf/profe/tabs/TabsActivity.kt
+++ app/src/main/java/edu/adf/profe/tabs/TabsActivity.kt
@@ -1,11 +1,14 @@
 package edu.adf.profe.tabs
 
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.addCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.tabs.TabLayoutMediator
+import edu.adf.profe.Constantes
 import edu.adf.profe.R
 import edu.adf.profe.databinding.ActivityTabsBinding
 
@@ -30,5 +33,34 @@ class TabsActivity : AppCompatActivity() {
             tl.text = "VISTA ${n+1}"
         }).attach()
 
+        onBackPressedDispatcher.addCallback{
+                Log.d(Constantes.ETIQUETA_LOG, "Ha tocado el botón hacia atrás 1")
+                haciaAtras()
+        }
     }
+    fun haciaAtras ()
+    {
+        if (this.binding.vpt.currentItem == 0)//si estoy en el tab 1
+        {
+            finish() //salgo
+        } else { //si no
+            this.binding.vpt.currentItem = this.binding.vpt.currentItem-1 //me voy al tab anterior
+        }
+    }
+
+//FORMA ANTIGUA DE ESCUCHAR EL BOTÓN HACIA ATRÁS
+    //A PARTIR DEL API 33, DEPRECADO
+/*
+    override fun onBackPressed() {
+        Log.d(Constantes.ETIQUETA_LOG, "Ha dado al botón de ir hacia atrás")
+        if (this.binding.vpt.currentItem == 0)//si estoy en el tab 1
+        {
+            super.onBackPressed() //salgo
+        } else { //si no
+            this.binding.vpt.currentItem = this.binding.vpt.currentItem-1 //me voy al tab anterior
+        }
+
+
+    }*/
+
 }
