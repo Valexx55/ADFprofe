591bf956f8050e12ab7e4e91d995f8b50a32afb3
Val <valexx55@gmail.com>
2025-10-10 12:18:12 +0200

    MOSTRANDO DATOS FB

--- app/src/main/java/edu/adf/profe/realtimedatabase/Cliente.kt
+++ app/src/main/java/edu/adf/profe/realtimedatabase/Cliente.kt
@@ -1,4 +1,4 @@
 package edu.adf.profe.realtimedatabase
 
 data class Cliente
-    (val edad: Int, val localidad:String, val nombre:String, val email:String, var clave:String="")
+    (val edad: Long, val localidad:String, val nombre:String, val email:String, var clave:String="")
--- app/src/main/java/edu/adf/profe/realtimedatabase/InsertarClientesFirebaseActivity.kt
+++ app/src/main/java/edu/adf/profe/realtimedatabase/InsertarClientesFirebaseActivity.kt
@@ -14,7 +14,9 @@ import edu.adf.profe.Constantes
 import edu.adf.profe.R
 import edu.adf.profe.databinding.ActivityInsertarClientesFirebaseBinding
 
-const val URL_REAL_TIME_DATABASE = "https://adfprofe-default-rtdb.europe-west1.firebasedatabase.app/"
+const val URL_REAL_TIME_DATABASE =
+    "https://adfprofe-default-rtdb.europe-west1.firebasedatabase.app/"
+
 class InsertarClientesFirebaseActivity : AppCompatActivity() {
 
     lateinit var databaseReference: DatabaseReference //es el objeto para manejar la bd
@@ -35,10 +37,12 @@ class InsertarClientesFirebaseActivity : AppCompatActivity() {
 
     fun crearClienteFB(view: View) {
 
-        val cliente = Cliente(binding.edadCliente.text.toString().toInt(),
+        val cliente = Cliente(
+            binding.edadCliente.text.toString().toLong(),
             binding.localidadCliente.text.toString(),
             binding.nombreCliente.text.toString(),
-            binding.emailCliente.text.toString())
+            binding.emailCliente.text.toString()
+        )
 
         //generamos la clave
         val idCliente = this.databaseReference.push().key
@@ -46,13 +50,47 @@ class InsertarClientesFirebaseActivity : AppCompatActivity() {
 
         //insertamos en bd remota
         this.databaseReference.child("clientes").child(cliente.clave).setValue(cliente)
-            .addOnCompleteListener{
-                tarea ->
+            .addOnCompleteListener { tarea ->
                 Toast.makeText(this, "CLIENTE INSERTADO", Toast.LENGTH_LONG).show()
             }.addOnFailureListener {
                 Toast.makeText(this, "ERROR INSERTANDO CLI", Toast.LENGTH_LONG).show()
                 Log.e(Constantes.ETIQUETA_LOG, "Error al insertar ", it)
             }
 
+    }
+
+    fun mostrarClientesFB(view: View) {
+        Log.d(Constantes.ETIQUETA_LOG, "Mostrar clientes FB")
+        this.databaseReference.child("clientes").get().addOnSuccessListener { datos ->
+
+            var clave = datos.key
+            Log.d(Constantes.ETIQUETA_LOG, "Clave $clave")
+            var lista = datos.value as Map<String, Map<String, Any>>
+            var entradas = lista.entries
+            var ncliens = entradas.size
+            Log.d(Constantes.ETIQUETA_LOG, "Hay $ncliens clientes")
+            var listaClientes = ArrayList<Cliente>()
+            var cliente: Cliente
+            //para cada cliente, obtenemos los datos
+            entradas.forEach { (claveId, valor) ->
+                Log.d(Constantes.ETIQUETA_LOG, "idCliente = $claveId")
+                Log.d(Constantes.ETIQUETA_LOG, "nombre = ${valor.get("nombre")}")
+                Log.d(Constantes.ETIQUETA_LOG, "email = ${valor.get("email")}")
+                Log.d(Constantes.ETIQUETA_LOG, "localidad = ${valor.get("localidad")}")
+                Log.d(Constantes.ETIQUETA_LOG, "edad = ${valor.get("edad")}")
+                cliente = Cliente(
+                    valor.get("edad") as Long,
+                    valor.get("localidad").toString(),
+                    valor.get("nombre").toString(),
+                    valor.get("email").toString(),
+                    claveId
+                )
+
+                listaClientes.add(cliente)
+                if (listaClientes.size == ncliens) {
+                    listaClientes.forEachIndexed { n, c -> Log.d(Constantes.ETIQUETA_LOG, "Cliente $n = $c.toString()")}
+                }
+            }
+        }
     }
 }
